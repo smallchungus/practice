9f3cb4a4807c56138f7eb50af56877319ccafaaa
smallchungus <wchen1396@gmail.com>
2023-04-19 10:28:43 -0400

    maxSubarray

--- max_subarray.py
+++ max_subarray.py
@@ -7,3 +7,11 @@ class Solution:
         for num in nums:
             current_max = max ( num, current_max + num )
             final_max = max(current_max, final_max)
+
+        return final_max
+
+
+
+s = Solution()
+print(s.max_subarray([-2,1,-3,4,-1,2,1,-5,4]))
+#answer is 6
