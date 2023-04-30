834520375809ddccf0aae72e02ad10892f293bda
smallchungus <wchen1396@gmail.com>
2023-04-30 15:00:06 -0400

    add print course

--- tutorial.py
+++ tutorial.py
@@ -1,4 +1,14 @@
-birth_year = input('Birth year:')
-print(type(birth_year))
-age = 2023 - int(birth_year)
-print(age)
+# birth_year = input('Birth year:')
+# print(type(birth_year))
+# age = 2023 - int(birth_year)
+# print(age)
+#
+# weight_lbs = input('Weight (lbs):')
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+course = "Python's for Beginners"
+print(course)
+
+couse = 'Python for "Beginners"'
+print(course[-2])
